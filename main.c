1f4757eb76f577db9ae30091a6679c8d0080578f
Amine El Hafidi <aminel-h@c3r9p6.1337.ma>
2026-05-11 16:27:35 +0100

    add the main file which have the coder routine implementation and the main function

--- main.c
+++ main.c
@@ -1,48 +1,19 @@
-#include <pthread.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-typedef struct  s_dongle
-{
-    pthread_mutex_t mutex;
-} t_dongle;
-
-typedef struct s_data
-{
-    t_coder     *coder;
-    int     time_to_compile;
-    int     time_to_debug;
-    int     time_to_refactor;
-    int     time_to_burnout;
-    int     dongle_cooldown;
-} t_data;
-
-typedef struct   s_coder
-{
-    int     id;
-    pthread_t thread;
-    long    last_compile;
-    int     nb_compile;
-    t_dongle    *left;
-    t_dongle    *right;
-} t_coder;
+#include "codexion.h"
 
 void    *coder_routine(void *arg)
 {
-    t_data *data = (t_data *)arg;
+    t_coder *coder;
 
-    pthread_mutex_lock(&data->coder->left->mutex);
-    pthread_mutex_lock(&data->coder->right->mutex);
-    printf("coder %d is compiling now\n", data->coder->id);
-    sleep(data->time_to_compile);
-    sleep(data->dongle_cooldown);
-    pthread_mutex_unlock(&data->coder->left->mutex);
-    pthread_mutex_unlock(&data->coder->right->mutex);
-    printf("coder %d is debuging now\n", data->coder->id);
-    sleep(data->time_to_debug);
-    printf("coder %d is refactoring now\n", data->coder->id);
-    sleep(data->time_to_refactor);
+    coder = (t_coder *)arg;    
+    while (coder->simulation_running == 1)
+    {
+        if (!compile(coder))
+            break;
+        if (!debug(coder))
+            break;
+        if (!refactor(coder))
+            break;
+    }
     return NULL;
 }
 
@@ -56,17 +27,19 @@ int main()
     int     dongle_cooldown;
     char    *scheduler;
     int     i;
+    t_coder *coders;
+    t_dongle *dongles;
+    t_monitor   monitor;
     
-    time_to_compile = 5;
-    time_to_debug = 5;
-    time_to_refactor = 5;
-    time_to_burnout = 5;
-    dongle_cooldown = 5;
+    time_to_compile = 5000;
+    time_to_debug = 5000;
+    time_to_refactor = 5000;
+    time_to_burnout = 5000;
+    dongle_cooldown = 5000;
     scheduler = "EDF";
     n_coders = 10;
-    t_data data[n_coders];
-    t_coder *coders = malloc(sizeof(t_coder) * n_coders);
-    t_dongle *dongles = malloc(sizeof(t_dongle) * n_coders);
+    coders = malloc(sizeof(t_coder) * n_coders);
+    dongles = malloc(sizeof(t_dongle) * n_coders);
 
     i = 0;
     while (i < n_coders)
@@ -81,33 +54,37 @@ int main()
         coders[i].id = i;
         coders[i].left = &dongles[i];
         coders[i].right = &dongles[(i + 1) % n_coders];
-        i++;
-    }
-
-    // loop to generate data for all the threads
-    i = 0;
-    while (i < n_coders)
-    {
-        data[i].coder = &coders[i];
-        data[i].dongle_cooldown = dongle_cooldown;
-        data[i].time_to_burnout = time_to_burnout;
-        data[i].time_to_compile = time_to_compile;
-        data[i].time_to_debug = time_to_debug;
-        data[i].time_to_refactor = time_to_refactor;
+        coders[i].dongle_cooldown = dongle_cooldown;
+        coders[i].time_to_burnout = time_to_burnout;
+        coders[i].time_to_compile = time_to_compile;
+        coders[i].time_to_debug = time_to_debug;
+        coders[i].time_to_refactor = time_to_refactor;
+        coders[i].compiles_count = 0;
+        coders[i].simulation_running = 1;
         i++;
     }
 
     i = 0;
     while (i < n_coders)
     {
-        pthread_create(&coders[i].thread, NULL, coder_routine, &data[i]);
         i++;
     }
-    
+
+    i = 0;
+    while (i < n_coders)
+    {
+        pthread_create(&coders[i].thread, NULL, coder_routine, &coders[i]);
+        i++;
+    }
+    monitor.coders = coders;
+    monitor.n_coders = n_coders;
+    monitor.number_of_compiles_required = 3;
+    pthread_create(&monitor.monitor_thread_id, NULL, monitor_routine, &monitor);
     i = 0;
     while (i < n_coders)
     {
         pthread_join(coders[i].thread, NULL);
         i++;
     }
+    pthread_join(monitor.monitor_thread_id, NULL);
 }
